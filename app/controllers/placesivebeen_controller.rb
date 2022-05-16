12d5cb38ab9432f7e29a2041376a4d746f8feebc
afagan5 <abby.fagan@kellogg.northwestern.edu>
2022-05-16 23:04:37 +0000

    first draft

---
+++ app/controllers/placesivebeen_controller.rb
@@ -0,0 +1,2 @@
+class PlacesivebeenController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources "placesivebeen"
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
-end
+
